6f31af75ab30b0f02af1d04ca583da3b60fa969f
Thunder <mahmmoud.hassanein@gmail.com>
2023-07-15 15:04:21 +0300

    Works with signals.

--- app/app.go
+++ app/app.go
@@ -1,9 +1,12 @@
 package app
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	internal "github.com/Mahmoud-Emad/envserver/internal"
 	models "github.com/Mahmoud-Emad/envserver/models"
@@ -54,17 +57,39 @@ func NewApp(configFileName string) (app *App, err error) {
 }
 
 // Start starts the server and listens for incoming requests.
-func (a *App) Start() error {
-	log.Info().Msgf("Server is listening on http://%s:%d", a.Server.Host, a.Server.Port)
+func (a *App) Start() {
+	// Create a channel to listen for OS signals
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
 
 	a.registerHandlers()
 
-	err := http.ListenAndServe(fmt.Sprintf("%s:%d", a.Server.Host, a.Server.Port), nil)
-	if err != nil {
-		return err
+	// Create a new server
+	server := &http.Server{
+		Addr: string(rune(a.Server.Port)),
 	}
+	// Start the server in a goroutine
+	go func() {
+		log.Info().Msgf("Server is listening on http://%s:%d", a.Server.Host, a.Server.Port)
+		if err := http.ListenAndServe(fmt.Sprintf("%s:%d", a.Server.Host, a.Server.Port), nil); err != nil && err != http.ErrServerClosed {
+			log.Error().Msgf("Server failed to start: %v", err)
+		}
+		log.Info().Msg("Stopped serving new connections")
+	}()
 
-	return nil
+	// Wait for the shutdown signal
+	<-stop
+
+	// Create a context with a timeout for graceful shutdown
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// Shutdown the server
+	if err := server.Shutdown(ctx); err != nil {
+		log.Error().Msgf("Server shutdown error: %v", err)
+	} else {
+		log.Info().Msgf("Server gracefully stopped")
+	}
 }
 
 // registerHandlers registers all handlers with their respective paths in the HTTP router of this application's
