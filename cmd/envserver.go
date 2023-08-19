1c6b005e71b598c61bb2ae01dc4ae80f212e98b3
Mahmoud Emad <mahmmoud.hassanein@gmail.com>
2023-08-19 15:39:20 +0300

    Push to pull on another machine.

--- cmd/envserver.go
+++ cmd/envserver.go
@@ -1,15 +1,30 @@
-// // This file just for testing.
 package main
 
 import (
+	"flag"
+	"fmt"
+	"os"
+
 	envserver "github.com/Mahmoud-Emad/envserver/app"
 )
 
 func main() {
-	configFileName := "config.toml"
-	app, err := envserver.NewApp(configFileName)
-	if err != nil {
-		return
+	var configFilePath string
+	flag.StringVar(&configFilePath, "config", "", "Path to the configuration file")
+	flag.Parse()
+
+	if configFilePath == "" {
+		fmt.Println("Error: You must provide the path to the configuration file using the -config flag.")
+		flag.Usage()
+		os.Exit(1)
 	}
+
+	fmt.Println("configFilePath: ", configFilePath)
+	app, err := envserver.NewApp(configFilePath)
+	if err != nil {
+		fmt.Printf("Error creating the app: %s\n", err)
+		os.Exit(1)
+	}
+
 	app.Start()
 }
